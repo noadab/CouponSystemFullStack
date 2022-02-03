ea22a3efa35c3561a6cf2fe88b4394150fe68f70
noadab <noadabah1@gmail.com>
2022-02-03 11:56:12 +0200

    3.2

--- src/main/react/src/helper/fetchWrapper.js
+++ src/main/react/src/helper/fetchWrapper.js
@@ -1,34 +1,32 @@
 
-// const urlPrefix = "https://noa-project-f9caa-default-rtdb.firebaseio.com/";
-
-// import { useDispatch } from "react-redux";
-// import authActions from "../store/auth"
-
-export const FOOD_LIST = "food_list";
-export const INSPIRATION_LIST = "inspiration_says";
-
 const fetchWrapper = {
-  get: async (name, onfail)=> getHandler(name, onfail),
+  get: async (name)=> getHandler(name),
   fetch: async (method, name, dataToSend, onfail)=> putHandler(method, name, dataToSend, onfail),
   delete: async (method, name, token, onFail)=>deleteHandler(method, name, token, onFail)
 }
- 
+
+const logout=()=>{
+  localStorage.removeItem('token');
+  localStorage.removeItem('isLoggedIn');
+  localStorage.removeItem('userType');
+}
 
 
 const OK="Ok";
+// Custome exception
 const NOT_FOUND="Not found"
+// Other exceptions
 const METHOD_NOT_ALLOWED="Method not allowed"
+// Session end
 const BAD_REQUEST="Bad request"
 
-async function getHandler(name, onFail)
+async function getHandler(name)
 {
-
-  try {
     const response = await fetch(name);
     console.log(response)
     if (response.statusText === BAD_REQUEST) {
       alert(await response.text());
-      //dispatch(authActions.logout());
+      logout();
       throw new Error(response.statusText);
     } 
     else if (!response.ok) {
@@ -38,52 +36,45 @@ async function getHandler(name, onFail)
     const data = await response.json();
     console.log("fetchWrapper got data, len=" + Object.keys(data).length);
     return data;
-  } catch (error) {
-    onFail();
-    return error;
-  }
+  
 };
 
 async function putHandler(method, name, dataToSend, token, onFail=()=>{})
 {
-  try {
-    const requestOptions = {
+  
+  const requestOptions = {
       method: method,
       headers: { "Content-Type": "application/json",  token: token},
       body: JSON.stringify(dataToSend)
     };
     const response = await fetch(name, requestOptions);
     if (response.statusText === BAD_REQUEST) {
+      logout();
       alert(await response.text());
       throw new Error(await response.text());
     } 
 
     else if (!response.ok) {
       alert(await response.text())
-      throw new Error(await response.text());
+      throw new Error(response.json());
     }
+
     alert(await response.text());
-    const data = await response.json();
-    console.log("fetchWrapper."+method+", "+name+" got data, len=" + Object.keys(data).length);
-    return JSON.stringify(data);
-  } catch (error) {
-    onFail();
-    return error;
-    
-  }
+    return ;
+ 
 };
 
 async function deleteHandler(method, name, token, onFail=()=>{})
 {
-  try {
+  
     const requestOptions = {
       method: method,
       headers: { "Content-Type": "application/json",  token: token}
     };
     const response = await fetch(name, requestOptions);
     if (response.statusText === BAD_REQUEST) {
+      logout();
       alert(await response.text());
-      //dispatch(authActions.logout());
       throw new Error(await response.text());
     }
     
@@ -92,12 +83,9 @@ async function deleteHandler(method, name, token, onFail=()=>{})
       throw new Error(await response.text());
     }
     alert(await response.text());
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    onFail();
-    return error;
+    return;
+  
   }
-};
+
 
 export default fetchWrapper;
